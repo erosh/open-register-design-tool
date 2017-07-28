a9814de8e52cdb04a8d9353d13720cfabdcef659
Pavel Eroshenko <peroshenko@charlesworthresearch.com>
2017-07-28 20:09:21 +0300

    MyDebugController added

---
+++ src/ordt/extract/MyDebugController.java
@@ -0,0 +1,19 @@
+package ordt.extract;
+
+public class MyDebugController extends DebugController {
+
+    @Override
+    public boolean isActive() {
+        return false;
+    }
+
+    @Override
+    public void addAnnotations() {
+
+    }
+
+    @Override
+    public void setRunInfo() {
+
+    }
+}
